f72cd54c1a0f4c81af8a466215daa0fd26f224f6
0moknuni <kkys0456@gmail.com>
2022-09-23 20:28:40 +0900

    test!!!

---
+++ 2292/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindRoom(int.Parse(Console.ReadLine())));
+        }
+
+        static int FindRoom(int num)
+        {//Github test
+            int i = 0;
+            int roomNum = 1;
+            do
+            {
+                roomNum += 6 * i;
+                i++;
+            } while (roomNum < num);
+            
+            return i;
+        }
+    }
+}
